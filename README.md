62caabfb90eb51a2e5061cc93ebf05a6c2c03631
Mario Zechner <badlogicgames@gmail.com>
2021-09-25 13:24:55 +0200

    Ready for the 1.0 release.

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # buckety-buckeroo
 Got streaming data and want to create an approximate histogram for it? Want to also render the bins to a canvas without bells and whistles? That's what buckety-buckeroo does for you.
 
+The implementation is based on the paper [Streaming Parallel Decision Trees](http://jmlr.org/papers/volume11/ben-haim10a/ben-haim10a.pdf).
 
 # Demo
+![demo.png](demo.png)
+
+[Online demo](https://marioslab.io/dump/buckety-buckeroo)
+[Source](https://github.com/badlogic/buckety-buckeroo/blob/main/index.html)
 
 # Usage
 You can include `buckety-buckeroo` directly on your page via.
@@ -24,10 +29,52 @@ Then import all exported functions and classes in your source files via:
 import * as buckety from "@marioslab/buckety-buckeroo"
 ```
 
+Create a histogram and specify the number of buckets it should have:
+
+```
+let histogram = new buckety.Histogram(100);
+```
+
+Then add values:
+
+```
+histrogram.addValue(432.32);
+histrogram.addValue(-0.21);
+histrogram.addValue(64);
+```
+
+You can calculate approximate quantiles:
+
+```
+let tenPercentQuantile = histogram.quartile(0.1);
+```
+
+Or perform your own calculation by iterating the buckets:
+
+```
+let mean = 0;
+for (let bucket of histogram.buckets) {
+	mean += bucket.value;
+}
+mean /= histogram.buckets.length;
+```
+
+Finally, you can draw the histogram to a canvas.
+
+```
+<canvas></canvas>
+<script>
+	let canvas document.querySelector("canvas");
+	let histogram = new buckety.Histogram(20);
+	let histogramChart = new buckety.HistrogramChart(canvas, { histrogram: histogram });
+	histrogramChart.render();
+</script>
+```
+
+See the demo above for an interactive example.
 
 # Will you add feature $X
-No.
-
+No, but I might accept pull requests.
 
 # Development
 Install Git, Node.js, and Visual Studio Code. Then:
@@ -39,4 +86,4 @@ npm run dev
 code.
 ```
 
-Press `F5`, modify code, press `CTRL+S`, see the page auto-update with your changes, set some breakpoints. Just do your programmer thing.
+Press `F5`, modify code, press `CTRL+S`, see the page auto-update with your changes, set some breakpoints.
--- index.ts
+++ index.ts
@@ -1,7 +1,3 @@
-export interface StringMap<T> {
-	[key: string]: T;
-}
-
 function updateCanvasRenderBufferSize(canvas: HTMLCanvasElement) {
 	let dpr = window.devicePixelRatio;
 	let w = canvas.clientWidth * dpr;
@@ -12,3 +8,164 @@ function updateCanvasRenderBufferSize(canvas: HTMLCanvasElement) {
 	}
 }
 
+export interface HistogramBucket {
+	value: number;
+	count: number;
+}
+
+export class Histogram {
+	buckets: HistogramBucket[] = [];
+	totalCount = 0;
+
+	constructor (public readonly numBuckets = 64) {
+	}
+
+	addSample(value: number) {
+		let buckets = this.buckets;
+		this.totalCount++;
+
+		for (let i = 0; i < buckets.length; i++) {
+			let bucket = buckets[i];
+			if (bucket.value == value) {
+				bucket.count++;
+				return;
+			}
+
+			if (bucket.value > value) {
+				this.insertSorted(value);
+				this.trim();
+				return;
+			}
+		}
+
+		this.buckets.push({ value: value, count: 1 });
+		this.trim();
+	}
+
+	quantile(quantile: number) {
+		let buckets = this.buckets;
+		let count = quantile * this.totalCount;
+		for (let i = 0; i < buckets.length; i++) {
+			count -= buckets[i].count;
+			if (count <= 0) {
+				return buckets[i].value;
+			}
+		}
+		return -1;
+	}
+
+	private insertSorted(value: number) {
+		let buckets = this.buckets;
+		let min = 0;
+		let max = buckets.length;
+		var index = Math.floor((min + max) / 2);
+		while (max > min) {
+			let arrayValue = buckets[index].value;
+			if (value < arrayValue) {
+				max = index;
+			} else {
+				min = index + 1;
+			}
+			index = Math.floor((min + max) / 2);
+		}
+		buckets.splice(index, 0, { value: value, count: 1 });
+	};
+
+	private trim() {
+		let buckets = this.buckets;
+		while (buckets.length > this.numBuckets) {
+
+			let minDelta = Number.POSITIVE_INFINITY;
+			let minIndex = 0;
+
+			for (let i = 0; i < buckets.length; i++) {
+				if (i == 0) continue;
+				let delta = buckets[i].value - buckets[i - 1].value;
+				if (delta < minDelta) {
+					minDelta = delta;
+					minIndex = i;
+				}
+			}
+
+			let totalCount = buckets[minIndex - 1].count + buckets[minIndex].count;
+			buckets[minIndex] = {
+				value: (buckets[minIndex - 1].value * buckets[minIndex - 1].count + buckets[minIndex].value * buckets[minIndex].count) / totalCount,
+				count: totalCount
+			}
+			buckets.splice(minIndex - 1, 1);
+		}
+	}
+}
+
+export interface HistogramChartConfig {
+	histogram: Histogram,
+	padding?: number,
+	backgroundColor?: string,
+	barColor?: string,
+	hoverBarColor?: string,
+	showInfoOnHover?: boolean,
+	infoColor?: string
+	infoSize?: number
+}
+
+export class HistogramChart {
+	private ctx: CanvasRenderingContext2D;
+	private mouseX;
+
+	constructor (public canvas: HTMLCanvasElement, public config: HistogramChartConfig) {
+		if (!config.padding) config.padding = 18;
+		if (!config.backgroundColor) config.backgroundColor = "lightgray";
+		if (!config.barColor) config.barColor = "lightgreen";
+		if (!config.hoverBarColor) config.hoverBarColor = "green";
+		if (config.showInfoOnHover == undefined) config.showInfoOnHover = true;
+		if (!config.infoColor) config.infoColor = "black";
+		if (config.infoSize == undefined) config.infoSize = 18;
+		this.ctx = canvas.getContext("2d");
+
+		canvas.addEventListener("mousemove", e => {
+			this.mouseX = e.offsetX / canvas.clientWidth * canvas.width;
+		});
+	}
+
+	render() {
+		updateCanvasRenderBufferSize(this.canvas);
+		let ctx = this.ctx;
+		let hist = this.config.histogram;
+		let buckets = hist.buckets;
+		let w = this.canvas.width;
+		let h = this.canvas.height;
+		let padding = this.config.padding;
+
+		let minCount = Number.POSITIVE_INFINITY;
+		let maxCount = Number.NEGATIVE_INFINITY;
+		for (let bucket of buckets) {
+			minCount = Math.min(minCount, bucket.count);
+			maxCount = Math.max(maxCount, bucket.count);
+		}
+		let bucketWidth = (w - padding * 2) / buckets.length;
+		let bucketHeightScale = (h - padding * 2) / maxCount;
+
+		let mouseHoverBucket = buckets[Math.floor(Math.max(padding, Math.min(w - padding, this.mouseX)) / bucketWidth)];
+		if (this.mouseX < padding) mouseHoverBucket = null;
+
+		ctx.fillStyle = this.config.backgroundColor;
+		ctx.fillRect(0, 0, w, h);
+
+		let x = padding;
+		for (let i = 0; i < buckets.length; i++, x += bucketWidth) {
+			let bucket = buckets[i];
+			let bucketHeight = bucket.count * bucketHeightScale;
+			if (bucket == mouseHoverBucket) ctx.fillStyle = this.config.hoverBarColor;
+			else ctx.fillStyle = this.config.barColor;
+			ctx.fillRect(x, h - bucketHeight, bucketWidth, bucketHeight);
+		}
+
+		if (mouseHoverBucket) {
+			let fontSize = this.config.infoSize * window.devicePixelRatio;
+			ctx.font = `${fontSize}px Arial`;
+			ctx.fillStyle = this.config.infoColor;
+			ctx.fillText(`value: ${mouseHoverBucket.value}`, padding, padding + fontSize);
+			ctx.fillText(`count: ${mouseHoverBucket.count}`, padding, padding + fontSize * 2);
+		}
+	}
+}
